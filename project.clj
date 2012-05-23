ee9faeeeea7f62805ba4aff8ac9256e6feeeb12a
Zaiste! λ <oh@zaiste.net>
2012-05-23 18:20:14 +0200

    hiccup congomonog deps

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject inprogrez "0.1.0-SNAPSHOT"
             :description "FIXME: write this!"
             :dependencies [[org.clojure/clojure "1.4.0"]
+                           [hiccup "1.0.0"]
+                           [congomongo "0.1.9"]
                            [noir "1.3.0-beta3"]]
             :main inprogrez.server)
 
